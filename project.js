625be890b63d9d84d1c3a67ab04b083199b1141c
shuhampatel20 <sp0200838patel@gmail.com>
2024-12-07 12:12:35 +0530

    about postman working

--- project.js
+++ project.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const users = require("./MOCK_DATA.json")
+const fs = require("fs");
+const { error } = require("console");
 
 const app = express();
 
+
+// middleware - plugin
+
+app.use(express.urlencoded({extended: false}));
 // browser html data
 
 app.get("/users",(req,res)=>{
@@ -22,6 +28,7 @@ app
 .get((req,res)=>{
     const id = Number(req.params.id);
     const user = users.find((user)=> user.id===id);
+    if(!user) return res.status(404).json({error: "user not found"})
     return res.json(user);
 })
 .patch((req,res)=>{
@@ -31,6 +38,21 @@ app
 .delete((req,res)=>{
     // delete user with user id 
     return res.json({status:"pending"});
-})
+});
+
+app.post("/api/users",(req,res)=>{
+    // to create a new user
+    const body = req.body;
+    if(!body || !body.first_name || !body.last_name || !body.email ||!body.gender ||!body.job_tittle){
+        return res.status(400).json("reqiured all field to be inserted");
+    }
+    users.push({...body,id:users.length+1});
+
+    fs.writeFile("./MOCK_DATA.json",JSON.stringify(users),(err,data)=>{
+        return res.status(201).json({status:"success",id:users.length});
+    });
+
+    
+});
 
 app.listen(8000,()=> console.log("project started"));
